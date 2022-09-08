0fcb6d2616db3c0299b2ce03ac44be4ea5436e6d
Brijk55 <101750353+Brijk55@users.noreply.github.com>
2022-09-08 18:47:47 +0100

    Stg4

--- src/main/java/com/teamtreehouse/stormy/MainActivity.java
+++ src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import okhttp3.Call;
@@ -20,6 +23,7 @@ import okhttp3.internal.tls.OkHostnameVerifier;
 public class MainActivity extends AppCompatActivity {
 
   public static final String TAG = MainActivity.class.getSimpleName();
+  private CurrentWeather currentWeather;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -50,15 +54,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onResponse(Call call, Response response) throws IOException {
           try {
-            Log.v(TAG, response.body().string());
+            String jsonData = response.body().string();
+            Log.v(TAG, jsonData);
             if (response.isSuccessful()) {
+              currentWeather = getCurrentDetails(jsonData);
 
             } else {
               alertUserAboutError();
             }
           } catch (IOException e) {
             Log.e(TAG, "IO Exception caught: ", e);
+          } catch (JSONException e) {
+            Log.e(TAG, "JSON Exception caught: ", e);
           }
+
         }
       });
     }
@@ -67,6 +76,31 @@ public class MainActivity extends AppCompatActivity {
           Toast.LENGTH_LONG).show();
     }
     Log.d(TAG, "Main UI code is running, hooray!");
+  }
+
+  private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+    JSONObject forecast = new JSONObject(jsonData);
+
+    String timezone = forecast.getString("timezone");
+    Log.i(TAG, "From JSON: " + timezone);
+
+    JSONObject currently = forecast.getJSONObject("currently");
+
+    CurrentWeather currentWeather = new CurrentWeather();
+
+    // Parse weather data from currently object
+    currentWeather.setHumidity(currently.getDouble("humidity"));
+    currentWeather.setTime(currently.getLong("time"));
+    currentWeather.setIcon(currently.getString("icon"));
+    currentWeather.setLocationLabel("Alcatraz Island, CA");
+    currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+    currentWeather.setSummary(currently.getString("summary"));
+    currentWeather.setTemperature(currently.getDouble("temperature"));
+    currentWeather.setTimeZone(timezone);
+
+    Log.d(TAG, currentWeather.getFormattedTime());
+
+    return currentWeather;
   }
 
   private boolean isNetworkAvailable() {
