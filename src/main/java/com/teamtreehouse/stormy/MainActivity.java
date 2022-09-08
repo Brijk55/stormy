b2852e0c8ac46edb94b80696372a3bfeb95f5188
Brijk55 <101750353+Brijk55@users.noreply.github.com>
2022-09-08 18:46:09 +0100

    Stg3

--- src/main/java/com/teamtreehouse/stormy/MainActivity.java
+++ src/main/java/com/teamtreehouse/stormy/MainActivity.java
@@ -1,15 +1,21 @@
 package com.teamtreehouse.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.internal.tls.OkHostnameVerifier;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,32 +26,64 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-
     String apiKey = "57eaf3aa961968bf65b0619680588073";
     double latitude = 37.8267;
-    double longitude = -122.423;
+    double longitude = -122.4233;
 
-    String forecastURL = "https://api.forecast.io/forecast/"
-        + apiKey + "/"
-        + latitude + ","
-        + longitude;
+    String forecastURL = "https://api.darksky.net/forecast/" + apiKey +
+        "/" + latitude + "," + longitude;
 
-    OkHttpClient client = new OkHttpClient();
+    if (isNetworkAvailable()) {
+      OkHttpClient client = new OkHttpClient();
 
-    Request request = new Request.Builder()
-        .url(forecastURL)
-        .build();
+      Request request = new Request.Builder()
+          .url(forecastURL)
+          .build();
 
-    Call call = client.newCall(request);
+      Call call = client.newCall(request);
+      call.enqueue(new Callback() {
+        @Override
+        public void onFailure(Call call, IOException e) {
 
-    try {
-      Response response = call.execute();
-      if (response.isSuccessful()) {
-        Log.v(TAG, response.body().string());
-      }
-    } catch (IOException e) {
-      Log.e(TAG, "IO Exception caught: ", e);
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException {
+          try {
+            Log.v(TAG, response.body().string());
+            if (response.isSuccessful()) {
+
+            } else {
+              alertUserAboutError();
+            }
+          } catch (IOException e) {
+            Log.e(TAG, "IO Exception caught: ", e);
+          }
+        }
+      });
+    }
+    else {
+      Toast.makeText(this, R.string.network_unavailable_message,
+          Toast.LENGTH_LONG).show();
+    }
+    Log.d(TAG, "Main UI code is running, hooray!");
+  }
+
+  private boolean isNetworkAvailable() {
+    ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+    boolean isAvailable = false;
+
+    if (networkInfo != null && networkInfo.isConnected()) {
+      isAvailable = true;
     }
 
+    return isAvailable;
+  }
+
+  private void alertUserAboutError() {
+    AlertDialogFragment dialog = new AlertDialogFragment();
+    dialog.show(getFragmentManager(), "error_dialog");
   }
 }
